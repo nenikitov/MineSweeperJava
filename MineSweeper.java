b97556bea4954b08eb075253ca563dfd10134fc4
nenikitov <nenikitov@mail.ru>
2021-04-23 00:04:44 -0400

    Created TODO

--- MineSweeper.java
+++ MineSweeper.java
@@ -1,4 +1,12 @@
 public class MineSweeper {
+	/* TODO
+	 * Separate the input handling logic in separate class
+	 * Player actions should have more general arguments rather than tileX and tileY
+	 * Execute action method for the mineField
+	 * Enum attributes https://www.baeldung.com/java-enum-values for player actions
+	 * Switch expressions with -> https://docs.oracle.com/en/java/javase/13/language/switch-expressions.html
+	*/
+	
     public static void main(String[] args) {
         MineField mineField = new MineField(10, 5, 0.25);
 
