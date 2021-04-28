e3c1b5bea73e5eb252628f17c86715379dbca04e
nenikitov <nenikitov@mail.ru>
2021-04-28 08:56:00 -0400

    Added regions

--- Player.java
+++ Player.java
@@ -1,6 +1,9 @@
 public class Player {
+    //#region Fields
     private int lives;
+    //#endregion
 
+    //#region Constructors
     /**
      * Initialize the player
      * @param lives The number of lives that the player has
@@ -8,7 +11,9 @@ public class Player {
     public Player(int lives) {
         this.lives = lives;
     }
+    //#endregion
 
+    //#region Other
     /**
      * Subtract a live and return if game should be over
      * @return True - game should be over, false - game should continue
@@ -17,4 +22,5 @@ public class Player {
         this.lives--;
         return this.lives <= 0;
     }
+    //#endregion
 }
