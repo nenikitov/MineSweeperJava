b63cdcb9b42dbf206e9e4efe90f472dab4b57870
nenikitov <nenikitov@mail.ru>
2021-04-28 08:59:09 -0400

    Added regions

--- TileStates.java
+++ TileStates.java
@@ -1,20 +1,28 @@
 public enum TileStates {
+    //#region enum entries
     CLOSED           ('■'),
     OPENED           (' '),
     MARKED_FLAG      ('P'),
     MARKED_QUESTION  ('?'),
     EXPLODED         ('*');
+    //#endregion
 
+    //#region Fields
     private final char displayChar;
+    //#endregion
 
+    //#region Constructors
     private TileStates(char displayChar) {
         this.displayChar = displayChar;
     }
+    //#endregion
 
+    //#region Other
     /**
      * Convert the tile type to character that will be displayed in the minefield
      */
     public String toString() {
         return Character.toString(this.displayChar);
     }
+    //#endregion
 }
