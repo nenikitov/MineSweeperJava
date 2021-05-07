8a4f396338ed57fc35805c1cec5697d39554d406
nenikitov <nenikitov@mail.ru>
2021-05-07 19:53:18 -0400

    Created a script to build the program

---
+++ compile.bat
@@ -0,0 +1,12 @@
+:: Recreate a build directory
+del  /q build/
+mkdir build/
+:: Build the program
+javac -d ./build/ *.java
+:: Create the manifest
+cd build/
+echo Main-Class: MineSweeper > Manifest.txt
+:: Build into jar
+jar -cfm MineSweeper.jar Manifest.txt *
+:: Create a .bat executable
+echo java -jar MineSweeper.jar > MineSweeper.bat
